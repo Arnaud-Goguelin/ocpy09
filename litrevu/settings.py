5546e9f09cf0737dd6c81cce9400edcfdde81fab
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-08-28 23:52:16 +0200

    feat(templates): start to implement templates html and css architecture

--- litrevu/settings.py
+++ litrevu/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = "litrevu.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+                BASE_DIR / 'templates',
+            ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
