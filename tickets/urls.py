8df2629838aa573a8c4e2b4ae5851d3d0efda2f7
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-08-28 23:28:22 +0200

    feat(urls): complete urls in tickets app and litrevu app
    # Conflicts:
    #	tickets/urls.py

---
+++ tickets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'tickets'
+
+urlpatterns = [
+    path('', views.TicketListView.as_view(), name='list'),
+    path('<int:primary_key>/', views.TicketDetailView.as_view(), name='detail'),
+    path('create/', views.TicketCreateView.as_view(), name='create'),
+    path('<int:primary_key>/edit/', views.TicketUpdateView.as_view(), name='edit'),
+    path('<int:primary_key>/delete/', views.TicketDeleteView.as_view(), name='delete'),
+]
