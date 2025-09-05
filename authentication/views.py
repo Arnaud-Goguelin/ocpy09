b707c9148b0b0517791bd65005c64e780cd8b6a2
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-09-05 16:04:00 +0200

    feat(auth views): create custom user creation form for signup view

--- authentication/views.py
+++ authentication/views.py
@@ -6,6 +6,38 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import User
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Bob',
+            'id': 'id_username_signup'
+        })
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'complicated_secret_password',
+            'id': 'id_password_signup_1'
+        })
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'complicated_secret_password',
+            'id': 'id_password_signup_2'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
 
 class CustomLoginView(LoginView):
     template_name = 'authentication/login.html'
@@ -19,7 +51,7 @@ class CustomLogoutView(LogoutView):
 
 class SignUpView(CreateView):
     model = User
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = 'authentication/signup.html'
     success_url = reverse_lazy('authentication:login')
 
