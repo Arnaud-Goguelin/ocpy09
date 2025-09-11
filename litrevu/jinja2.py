5c2148e541a96aff5f770fc1b6eb3a3e049ca65e
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-09-12 00:23:03 +0200

    feat(litrevu jinja): update settings to handle jinja2 rendering

---
+++ litrevu/jinja2.py
@@ -0,0 +1,20 @@
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.middleware.csrf import get_token
+from django.urls import reverse
+from jinja2 import Environment
+
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update(
+        {
+            "static": staticfiles_storage.url,
+            "url": reverse,
+            "csrf_token": lambda request: get_token(request),
+        }
+    )
+    return env
+
+def user_context(request):
+    """Context processor pour rendre 'user' disponible dans Jinja2"""
+    return {'user': request.user if hasattr(request, 'user') else None}
