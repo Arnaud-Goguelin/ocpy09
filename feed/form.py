dbc194cbbefacac0a225fdfb55fec5f6e1d515bf
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-09-12 15:27:09 +0200

    feat(feed): create view with its custom form class and update urls in feep app for landing page (create + list)

---
+++ feed/form.py
@@ -0,0 +1,63 @@
+import logging
+
+from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import Subscription
+
+
+logger = logging.getLogger("feed")
+User = get_user_model()
+
+
+class CreateSubscriptionForm(forms.ModelForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Nom d'utilisateur à suivre",
+                "id": "id_subscription",
+            }
+        ),
+        label="Nom d'utilisateur",
+    )
+
+    class Meta:
+        model = Subscription
+        fields = []
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            user_to_follow = User.objects.get(username=username)
+            logger.info(f"User found: {username}")
+        except User.DoesNotExist as error:
+            logger.warning(f"Attempt from {self.user.username} to follow {username}")
+            raise forms.ValidationError("This user does not exist.") from error
+
+        if user_to_follow == self.user:
+            logger.warning(f"Attempt from {self.user.username} to follow itself.")
+            raise forms.ValidationError("You can not follow yourself.")
+
+        if self.user.check_if_following(user_to_follow):
+            logger.warning(f"Attempt from {self.user.username} to follow {username} twice.")
+            raise forms.ValidationError("You already follow this user.")
+
+        logger.info(f"Attempt successful from {self.user.username} to follow {username}")
+        return username
+
+    def save(self, commit=True):
+        username = self.cleaned_data["username"]
+        user_to_follow = User.objects.get(username=username)
+
+        subscription = Subscription(follower=self.user, followed=user_to_follow)
+
+        if commit:
+            subscription.save()
+            logger.info(f"Subscription created for {self.user.username} now following {username}")
+
+        return subscription
--- feed/views.py
+++ feed/views.py
@@ -1,1 +1,45 @@
-# Create your views here.
+import logging
+
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .form import CreateSubscriptionForm
+from .models import Subscription
+
+
+logger = logging.getLogger("feed")
+
+
+class SubscriptionLandingView(LoginRequiredMixin, CreateView):
+    template_name = "feed/subscription_landing.html"
+    model = Subscription
+    form_class = CreateSubscriptionForm
+    success_url = reverse_lazy("feed:subscriptions")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context.update(
+            {
+                "following": user.following.select_related("followed"),
+                "followers": user.followers.select_related("follower"),
+            }
+        )
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, f"You now follow {form.cleaned_data['username']} !")
+        logger.info(f"Attempt successful from {self.request.user.username} to follow {form.cleaned_data['username']}")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        logger.warning(f"Attempt from {self.request.user.username} to follow {form.cleaned_data['username']} failed.")
+        return super().form_invalid(form)
