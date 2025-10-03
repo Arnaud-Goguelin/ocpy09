1a99495fa030d9420a05e4e5adc4b50229db3565
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-10-03 12:34:16 +0200

    feat(management): add commands to create test users automatically and remeber password

---
+++ users/management/commands/create_test_users.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create test users'
+
+    def handle(self, *args, **options):
+        users_data = [
+            {'username': 'Bob', 'password': 'litrevuTest'},
+            {'username': 'Tom', 'password': 'litrevuTest'},
+            ]
+
+        for user_data in users_data:
+            user, created = User.objects.get_or_create(
+                username=user_data['username'],
+                defaults={
+                    'is_active': True
+                    }
+                )
+            if created:
+                user.set_password(user_data['password'])
+                user.save()
+                self.stdout.write(
+                    self.style.SUCCESS(f'User "{user.username}" created successfully')
+                    )
+            else:
+                self.stdout.write(
+                    self.style.WARNING(f'User "{user.username}" already exists')
+                    )
