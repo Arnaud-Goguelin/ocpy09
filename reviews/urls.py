f9baa72154900a620e5093a522b6532f5d0de845
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-10-03 16:52:19 +0200

    fix(reviews urls): use pk as url parameter instead of custom primary_key param

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,6 +7,6 @@ app_name = "reviews"
 
 urlpatterns = [
     path("create/<int:ticket_id>/", views.ReviewCreateView.as_view(), name="create"),
-    path("<int:primary_key>/edit/", views.ReviewUpdateView.as_view(), name="edit"),
-    path("<int:primary_key>/delete/", views.ReviewDeleteView.as_view(), name="delete"),
+    path("<int:pk>/edit/", views.ReviewUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.ReviewDeleteView.as_view(), name="delete"),
 ]
