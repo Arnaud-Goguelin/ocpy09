174b27cd2b5ed5930b7c7269f8ce5a402dbf8f22
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-10-17 13:30:57 +0200

    style: format file with ruff

--- feed/urls.py
+++ feed/urls.py
@@ -8,5 +8,5 @@ app_name = "feed"
 urlpatterns = [
     path("subscription/", views.SubscriptionLandingView.as_view(), name="subscriptions"),
     path("user_posts/", views.UserPostsView.as_view(), name="user_posts"),
-    path("", views.FeedPostsView.as_view(), name="feed_posts")
+    path("", views.FeedPostsView.as_view(), name="feed_posts"),
 ]
