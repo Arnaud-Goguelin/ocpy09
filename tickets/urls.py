ea68efab21e8579123d5f0ff67b87b620193534e
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-10-24 10:53:10 +0200

    fix(tickets urls and templates): fix edit route for tockets + reviews and pass pk as arg in links in templates

--- tickets/urls.py
+++ tickets/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("<int:pk>/edit/", views.TicketUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete/", views.TicketDeleteView.as_view(), name="delete"),
     path("create_with_review/", views.TicketReviewCreateView.as_view(), name="create_with_review"),
-    path("update_with_review/", views.TicketReviewUpdateView.as_view(), name="edit_with_review"),
+    path("<int:pk>/update_with_review/", views.TicketReviewUpdateView.as_view(), name="edit_with_review"),
 ]
